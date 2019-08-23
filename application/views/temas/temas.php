de23d15e3105641c2319da73abce92a2d297955f
ErikBrian7 <43246683+ErikBrian7@users.noreply.github.com>
2019-08-23 17:26:23 -0500

    vista de temas
    vista de temas para produccion

--- application/views/temas/temas.php
+++ application/views/temas/temas.php
@@ -1,70 +1,81 @@
 <div class="main">
-      
       <!-- End Section Tabs -->
-
-
       <div class="section section-pagination">
         <div class="container">
-
            <!-- Section Temas de interes -->
-
           <h3 class="title">Temas de Interes</h3>
-          <div class="row">
-            <div class="col-md-12">
-
-
-            <div id="tema1" class="card" onclick ="colorCambiar('tema1','checkTema1')" style="width: 20rem;-webkit-filter:grayscale(100%)">
-  <img class="card-img-top" src="https://tvseriescritic.files.wordpress.com/2016/10/stranger-things-bicycle-lights-children.jpg" alt="Card image cap">
-  <div class="card-body">
-  <h4 class="card-title">Transporte</h4>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <div class="form-check">
-    <label class="form-check-label">
-        <input id="checkTema1" class="form-check-input" type="checkbox" value="">
-        Seleccionar
-        <span class="form-check-sign">
-            <span class="check"></span>
-        </span>
-    </label>
-</div>
-  </div>
-</div>
-
-<div  id="tema2" class="card" onclick ="colorCambiar('tema2','checkTema2')" style="width: 20rem;-webkit-filter:grayscale(100%)"
-    -webkit-filter: grayscale(100%);">
-  <img class="card-img-top" src="http://hellomoto.com.co/wp/wp-content/uploads/2018/04/hellobogota_1-1024x687.png" alt="Card image cap">
-  <div class="card-body">
-  <h4 class="card-title">Transporte</h4>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <div class="form-check">
-    <label class="form-check-label">
-        <input id="checkTema2" class="form-check-input" type="checkbox" value="">
-        Seleccionar
-        <span class="form-check-sign">
-            <span class="check"></span>
-        </span>
-    </label>
-</div>
-  </div>
-</div>
 
 
 
 
-
-
-
-
-
-
-            </div>
-
-           
-          </div>
-          <br>
-          
+<div class="row">
+	
+  
+  <div class="col-md-4">
+    <div id="tema1" class="card" onclick ="colorCambiar('tema1','checkTema1')" style="width: 20rem;-webkit-filter:grayscale(100%)">
+      <img class="card-img-top" src="https://tvseriescritic.files.wordpress.com/2016/10/stranger-things-bicycle-lights-children.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h4 class="card-title">Transporte</h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <div class="form-check">
+               <label class="form-check-label">
+                <input id="checkTema1" class="form-check-input" type="checkbox" value="">
+                Seleccionar
+                <span class="form-check-sign">
+                    <span class="check"></span>
+                </span>
+               </label>
+              </div>
         </div>
-      </div>
-
-
     </div>
+	</div>
+
+	<div class="col-md-4">
+    <div id="tema2" class="card" onclick ="colorCambiar('tema2','checkTema2')" style="width: 20rem;-webkit-filter:grayscale(100%)">
+      <img class="card-img-top" src="https://tvseriescritic.files.wordpress.com/2016/10/stranger-things-bicycle-lights-children.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h4 class="card-title">Transporte</h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <div class="form-check">
+               <label class="form-check-label">
+                <input id="checkTema2" class="form-check-input" type="checkbox" value="">
+                Seleccionar
+                <span class="form-check-sign">
+                    <span class="check"></span>
+                </span>
+               </label>
+              </div>
+        </div>
+    </div>
+	</div>
+
+
+	<div class="col-md-4">
+    <div id="tema3" class="card" onclick ="colorCambiar('tema3','checkTema3')" style="width: 20rem;-webkit-filter:grayscale(100%)">
+      <img class="card-img-top" src="https://tvseriescritic.files.wordpress.com/2016/10/stranger-things-bicycle-lights-children.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h4 class="card-title">Transporte</h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <div class="form-check">
+               <label class="form-check-label">
+                <input id="checkTema3" class="form-check-input" type="checkbox" value="">
+                Seleccionar
+                <span class="form-check-sign">
+                    <span class="check"></span>
+                </span>
+               </label>
+              </div>
+        </div>
+    </div>
+	</div>
+
+
+
+
+</div>
+
+
+
+</div>
+</div>
+</div>
