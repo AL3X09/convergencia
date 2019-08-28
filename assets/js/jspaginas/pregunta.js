c734e65636d654a37f775c09b8467a6cd99e1076
AL3X09 <alexcisas@gmail.com>
2019-08-27 22:24:00 -0500

    pusd

--- assets/js/jspaginas/pregunta.js
+++ assets/js/jspaginas/pregunta.js
@@ -55,6 +55,7 @@ function listarTemaNombrexx(callback) {
 
 function listarPregunas(temaid) {
     //co
+    var lista = $("#listapreguntas");
     $.ajax({
         url: base_url + 'pregunta/listarPreguntasXTema',
         method: 'GET',
@@ -62,37 +63,46 @@ function listarPregunas(temaid) {
 
             $.each(response, function(i, v) {
 
+
                 cont = i + 1;
+                var conte = '<div class="accordion" id="accordion_' + cont + '">';
+                conte += '<div class="card">';
+                conte += '<div class="card-header" id="heading_' + cont + '">';
+                conte += '<h2 class="mb-0">';
+                conte += '<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse_' + cont + '" aria-expanded="false" aria-controls="collapse_' + cont + '">';
+                conte += v.nombre;
+                conte += '</button>';
+                conte += '</h2>';
+                conte += '</div>';
+                conte += '<div id="collapse_' + cont + '" class="collapse" aria-labelledby="heading_' + cont + '" data-parent="#listapreguntas">';
+                conte += '<div class="card-body" id="tema_' + v.id + '">';
+                //rrecorro preguntas
+                $.each(v.preguntas, function(ip, p) {
+                    //console.log(p);
+                    conte += '<div class="form-group">' +
+                        '<label for="exampleInputEmail1">' + p.pregunta + '</label>' +
 
+                        '</div>';
 
-                $("#listapreguntas").append(
-
-                    '<div class="accordion" id="accordion_' + cont + '">' +
-                    '<div class="card">' +
-                    '<div class="card-header" id="heading_' + cont + '">' +
-                    '<h2 class="mb-0">' +
-                    '<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse_' + cont + '" aria-expanded="false" aria-controls="collapse_' + cont + '">' +
-                    v.nombre +
-                    '</button>' +
-                    '</h2>' +
-                    '</div>' +
-                    '<div id="collapse_' + cont + '" class="collapse" aria-labelledby="heading_' + cont + '" data-parent="#listapreguntas">' +
-                    '<div class="card-body" id="tema_' + v.id + '">' +
-                    //rrecorro preguntas
-                    //$.each(v.preguntas, function(ip, p) {
-                    v.preguntas.forEach(function(word) {
-                        console.log(word);
-
-
+                    $.each(p.respuestas, function(iq, q) {
                         //console.log(p.pregunta);
-                        word.pregunta
-                    }) +
-                    '</div>' +
-                    '</div>' +
-                    '</div>'
-                )
+                        conte += '<div class="form-check form-check-radio">' +
+                            '<label class="form-check-label">' +
+                            '<input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" >' +
+                            '<span class="form-check-sign"></span>' +
+                            q.respuesta +
+                            '</label>' +
+                            '</div>'
+                        '</div>';
 
+                    })
+                })
+                conte += '</div>';
+                conte += '</div>';
+                conte += '</div>';
+                lista.append(conte);
             });
+
 
         }
     }).fail(function(jqXHR, textStatus, errorThrown) {
